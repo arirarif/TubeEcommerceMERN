69374aeee59a5e69ead2059cf17f590c3bcd2066
arirarif <arirarif876@gmail.com>
2025-04-02 09:02:15 +0600

    cart page funcitonal

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -55,6 +55,32 @@ const ShopContextProvider = (props) => {
   //   console.log(cartItems)
   // }, [cartItems])
 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems)
+    cartData[itemId][size] = quantity
+    setCartItems(cartData)
+  }
+
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for (const items in cartItems) {
+      let itemInfo = products.find((product) => product._id === items)
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItems[items][item]
+          }
+          else {
+            totalAmount += 0
+          }
+        } catch (error) {
+
+        }
+      }
+    }
+    return totalAmount
+  }
+
   const value = {
     products,
     currency,
@@ -65,7 +91,9 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
-    getCartCount
+    getCartCount,
+    updateQuantity,
+    getCartAmount
   };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
